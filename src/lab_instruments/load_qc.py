258bbf4d6779b1460e9b838507d22218cb1a1f90
Cristian Bourceanu <v.c.bourceanu@sms.ed.ac.uk>
2025-07-30 11:34:38 +0100

    Include julia dependencies in the package as data

--- src/lab_instruments/load_qc.py
+++ src/lab_instruments/load_qc.py
@@ -1,6 +1,5 @@
 import os
-
-os.path.dirname(__file__)
+from importlib.resources import files
 
 def get_git_root(path=None):
     if path is None:
@@ -13,13 +12,27 @@ def get_git_root(path=None):
             return None
         path = parent
 
+# Following is to use git root as the path that contains Julia dependency and lock files
+#git_root = get_git_root(os.path.dirname(__file__))
+#if git_root:
+#    os.environ["JULIA_PROJECT"] = git_root
+
+# Locate the directory containing Project.toml and Manifest.toml
+julia_toml_path = files("lab_instruments")
+
+# Set JULIA_PROJECT environment variable
+os.environ["JULIA_PROJECT"] = str(julia_toml_path)
+
+print("JULIIA_PROJECT set to:", os.environ["JULIA_PROJECT"])
 
 os.environ["PYTHON_JULIAPKG_PROJECT"] = os.path.abspath("./julia_env")
 from juliacall import Main as jl, convert as jlconvert
 
-git_root = get_git_root(os.path.dirname(__file__))
-if git_root:
-    os.environ["JULIA_PROJECT"] = git_root
+# Run Pkg.instantiate() and Pkg.resolve()
+jl.seval(f'using Pkg; Pkg.activate("{julia_toml_path}")')
+jl.seval("using Pkg")
+jl.seval("Pkg.instantiate()")
+#jl.seval("Pkg.resolve()")
 
 jl.seval('using QuantiCam')
 jl.seval('using Serde')
